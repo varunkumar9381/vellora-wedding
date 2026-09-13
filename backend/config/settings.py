a7536b8922798a24bb5f79b86abf0e77d685c83e
Varun Kumar <yeligetivarunkumar@gmail.com>
2026-09-13 21:31:28 +0530

    Fix static file serving with WhiteNoise

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,6 +31,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
